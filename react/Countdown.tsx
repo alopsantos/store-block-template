ae171baa37d6f59bf75b47a12118be4dffb8b920
alopsantos <anderson_lops@hotmail.com>
2021-01-07 19:21:30 -0300

    Start

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
+import "./styles";
 interface CountdownProps {}
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div />
-}
+  return (
+    <div className="Container">
+      <h1>Teste</h1>
+    </div>
+  );
+};
 
 Countdown.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
-  type: 'object',
+  title: "editor.countdown.title",
+  description: "editor.countdown.description",
+  type: "object",
   properties: {},
-}
+};
 
-export default Countdown
+export default Countdown;
